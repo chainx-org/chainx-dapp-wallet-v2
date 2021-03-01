781418490553307caab6074435581aea6cdd74de
吴wbh1328551759 <1328551759@qq.com>
2021-03-01 11:00:46 +0800

    create new branch of feature/electron

--- packages/apps-electron/src/electron/window.ts
+++ packages/apps-electron/src/electron/window.ts
@@ -28,29 +28,33 @@ export function createWindow(environment: string): Promise<unknown> {
 
 
   async function getRelease() {
-    const {data} = await axios.get('https://api.github.com/repos/chainx-org/chainx-dapp-wallet-v2/releases/latest');
-    const fileNameList: string[] = await data.assets.map((file: any) => file.name);
-    const fileNameToDownload: string | string[] = await fileNameList.filter((fileName: string) => (fileName.indexOf('.exe') !== -1 || fileName.indexOf('.dmg') !== -1) && fileName.indexOf('.blockmap') === -1);
-    const filesToDownload = await data.assets.filter((file: any) => file.name === fileNameToDownload[0] || file.name === fileNameToDownload[1]);
-    const latestVersion = data.tag_name.slice(1)
-    const fileOfWin = filesToDownload.find((file: any) => file.name.indexOf('.exe') !== -1)
-    const fileOfMac = filesToDownload.find((file: any) => file.name.indexOf('.dmg') !== -1)
+    try{
+      const {data} = await axios.get('https://api.github.com/repos/chainx-org/chainx-dapp-wallet-v2/releases/latest');
+      const fileNameList: string[] = await data.assets.map((file: any) => file.name);
+      const fileNameToDownload: string | string[] = await fileNameList.filter((fileName: string) => (fileName.indexOf('.exe') !== -1 || fileName.indexOf('.dmg') !== -1) && fileName.indexOf('.blockmap') === -1);
+      const filesToDownload = await data.assets.filter((file: any) => file.name === fileNameToDownload[0] || file.name === fileNameToDownload[1]);
+      const latestVersion = data.tag_name.slice(1)
+      const fileOfWin = filesToDownload.find((file: any) => file.name.indexOf('.exe') !== -1)
+      const fileOfMac = filesToDownload.find((file: any) => file.name.indexOf('.dmg') !== -1)
 
-    if (packageJson.version !== latestVersion) {
+      if (packageJson.version !== latestVersion) {
 
-      require('electron')
-        .dialog
-        .showMessageBox(win, {
-          title: '温馨提示',
-          message: `您访问的版本不是最新版本哦，如果可以，请使用我们的最新版本，点击下方按钮进入下载页面：`,
-          buttons: ['mac 下载入口', 'windows 下载入口']
-        }).then((index) => {
-        if (index.response === 0) {
-          shell.openExternal(fileOfWin.browser_download_url);
-        } else if (index.response === 1) {
-          shell.openExternal(fileOfMac.browser_download_url);
-        }
-      });
+        require('electron')
+          .dialog
+          .showMessageBox(win, {
+            title: '温馨提示',
+            message: `您访问的版本不是最新版本哦，如果可以，请使用我们的最新版本，点击下方按钮进入下载页面：`,
+            buttons: ['mac 下载入口', 'windows 下载入口']
+          }).then((index) => {
+          if (index.response === 0) {
+            shell.openExternal(fileOfWin.browser_download_url);
+          } else if (index.response === 1) {
+            shell.openExternal(fileOfMac.browser_download_url);
+          }
+        });
+      }
+    }catch (err) {
+      console.log(err)
     }
   }
 
