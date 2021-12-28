0abfd809ddb6874b1213f573533f85a5c6bf868a
hongchen0315 <876186496@qq.com>
2021-12-28 15:09:34 +0800

    fix: usesbtcAssets/assetLock

--- packages/page-accounts-chainx/src/Myview/useSbtcAssets.tsx
+++ packages/page-accounts-chainx/src/Myview/useSbtcAssets.tsx
@@ -12,30 +12,36 @@ export interface SbtcAssetsInfo {
 
 function useSbtcAssets(account: string, n = 0): SbtcAssetsInfo {
   const {api,isApiReady} = useApi();
-  const [lock, setLock] = useState()
   const [state, setState] = useState<SbtcAssetsInfo>({
     balance: 0,
     extra: null,
     isFrozen: false,
     sufficient: false,
-    locked: null
+    locked: 0
   });
   const [, setValue] = useLocalStorage('sbtcInfo');
 
   useEffect((): void => {
     async function getAssets(account: string): Promise<any> {
-      // if(isApiReady) {
-          const asset = await api.query.assets.account(1, account)
-          const assetLock = await api.query.xGatewayRecords.locks(account, 1)
-          setValue(asset)
-          setLock(assetLock)
-      // }
-      let current = {
-        balance: 0,
-        extra: null,
-        isFrozen: false,
-        sufficient: false,
-      } as SbtcAssetsInfo;
+      if(isApiReady) {
+        const asset = await api.query.assets.account(1, account)
+        const assetLock = await api.query.xGatewayRecords.locks(account, 1)
+        // setValue(asset)
+        let current = {
+          balance: 0,
+          extra: null,
+          isFrozen: false,
+          sufficient: false,
+          locked: 0
+        } as SbtcAssetsInfo;
+        current = Object.assign(asset, {
+          account: account,
+          assetName: 'sBTC',
+          locked: assetLock
+        });
+        setValue(JSON.stringify(current));
+        setState(current);
+      }
 
       // if (JSON.stringify(current) === '{}') {
       //   current = {
@@ -45,16 +51,7 @@ function useSbtcAssets(account: string, n = 0): SbtcAssetsInfo {
       //     sufficient: false,
       //   } as SbtcAssetsInfo;
       // }
-      
-      current = Object.assign(asset, {
-        account: account,
-        assetName: 'sBTC',
-        locked: lock || 0
-      });
-      setValue(JSON.stringify(current));
-      setState(current);
     }
-
     getAssets(account);
   }, [account, isApiReady, n]);
 
